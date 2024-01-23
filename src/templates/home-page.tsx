e2bd1ef64096a9fa500a7d843694589e7ab6f72a
Stephen Zhou <hi@hyoban.cc>
2024-01-23 19:45:04 +0800

    add swr demo

--- src/templates/home-page.tsx
+++ src/templates/home-page.tsx
@@ -1,12 +1,14 @@
 import { Link } from 'waku';
 
 import { Counter } from '../components/counter.js';
+import { SWRDemo } from '../components/swr-demo.js';
 
 export const HomePage = async () => {
   const data = await getData();
 
   return (
     <div>
+      <SWRDemo />
       <title>{data.title}</title>
       <h1 className="text-4xl font-bold tracking-tight">{data.headline}</h1>
       <p>{data.body}</p>
